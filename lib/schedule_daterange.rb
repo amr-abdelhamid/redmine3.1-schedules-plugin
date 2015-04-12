c1d2f719c6eb1bb13a0c5a984b6462a4a6704987
Julien Viard de Galbert <julien@vdg.blogsite.org>
2015-04-13 00:27:10 +0200

    fix default (uses first_wday)

--- lib/schedule_daterange.rb
+++ lib/schedule_daterange.rb
@@ -9,18 +9,27 @@ module Schedule
       def initialize(date, lang = current_language, period = :month, week_count = 1)
         @date = date
         set_language_if_valid lang
-        calendar = Redmine::Helpers::Calendar.new(@date, lang, period)
-        @startdt = calendar.startdt
-        @enddt = calendar.enddt
+        @calendar = Redmine::Helpers::Calendar.new(@date, lang, period)
+        @startdt = @calendar.startdt
+        @enddt = @calendar.enddt
         if :week == period
                 @enddt += (week_count-1) * 7
         end
-        @active=(@startdt..@enddt)
+        @active=(@startdt..@enddt).to_a
       end
+
+      def first_wday
+        @calendar.first_wday
+      end
+
+      def last_wday
+        @calendar.last_wday
+      end
+
 
       # TODO
       def set_opendays()
-        @active=(@startdt..@enddt-2)
+        @active=(@startdt..@enddt-2).to_a
       end
     end
 end
